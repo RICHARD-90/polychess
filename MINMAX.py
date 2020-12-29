9c3d1febb1f21532776c20db57ecc653f80de84d
zakari <mahabzakariyaou@gmail.com>
2020-12-29 14:25:39 +0100

    commit MINMAX

---
+++ MINMAX.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 10:17:10 2020
+
+@author: abdoumahamadouzakariyaou
+"""
+import chess 
+import random
+
+board=chess.Board()
+
+print(board)
+
+while(not board.is_game_over()):
+    moves=board.legal_moves
+    listMoves=[]
+    for move in moves:
+        listMoves.append(move)
+    next=listMoves[random.randint(0,moves.count()-1)]
+    board.push(next)
+    print(board)
+    print("")
+    
+    
+    
+"Besoin de ponderation et de sa fonction inverse"
+
+def MineMax(position):
+    POSITION=""
+    positionRecur=""
+    ponderations=methodeponderation(position) #renvoie soit juste la ponderation de la position courante(board.legal_moves is None) ou une liste de ponderation
+    ponderation=max(ponderations)
+    if ponderation==ponderations[0]:
+        POSITION=position #ne bouge pas
+    elif ponderation!=ponderations[0]:
+        POSITION=position(ponderation)# la il nous une fonction qui renvoie la position en fonction d'une seule ponderation
+    elif position.board.legal_moves==None:# doute si c'est position.board.legal_moves ou position.legal_moves
+        if True:
+           POSITION=position
+        else:
+            for pos in board.legal_moves: # tout les deplacements poblible a partir de la position donnee
+                POSITION=MineMax(pos)
+    return POSITION
+            
+        
+            
+print(MineMax("gc"))
+                    
+                    
+                
+            
+            
+        
+    
+
