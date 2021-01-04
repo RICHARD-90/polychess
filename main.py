95aece1b58f535a8102784e64bf367e7cc845494
N'DRI KOUADIO ANGE RICHARD N'DRI <72502866+RICHARD-90@users.noreply.github.com>
2021-01-04 10:30:16 +0100

    Update main.py
    main version 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import chess
+board = chess.Board()
+# importer le fichier __init__()
 # tant que le jeu n'est pas terminé, on joue
 tour = 0 # permet de determiner a qui le tour
 while not(board.is_game_over()):
@@ -10,16 +13,27 @@ while not(board.is_game_over()):
         if move == deplacement[0]:
             cap_K = False
             print(deplacement[0],deplacement[1])
-  if cap_K:
-    appel a alphabeta
-  move_util = input('entrer votre movement:')
-  board.push(chess.Move.from_uci(str(move_util)))
+    if cap_K:
+        pass
+       #appel a alphabeta
+    move_util = input('entrer votre movement:')
+    board.push(chess.Move.from_uci(str(move_util)))
         
   else:
-    ******
+    moves = board.legal_moves
+    for move in moves :
+      for deplacemennt in polyglot():
+        if move == deplacement[0]:
+            cap_K = False
+            print(deplacement[0],deplacement[1])
+    if cap_K:
+        pass
+       #appel a alphabeta
+    move_util = input('entrer votre movement:')
+    board.push(chess.Move.from_uci(str(move_util)))
   tour = tour +1 
 
-# condition d'arret
-if (board.is_game_over()):
-    print("The game is over")
-    print(board.result())
+   # condition d'arret
+   if (board.is_game_over()):
+     print("The game is over")
+     print(board.result())
