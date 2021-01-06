423f8904e2911064231be7710c3900f6b33273c8
zakari <mahabzakariyaou@gmail.com>
2021-01-06 08:28:09 +0100

    ajout 3em version de  MINMAX

--- MINMAX.py
+++ MINMAX.py
@@ -63,8 +63,45 @@ def MineMax(etatcourant,prof,boolean):
             etatcourant=board.push(chess.Move.from_uci(move))
             valeur=min(valeur,MineMax(child,prof-1,True))
         return valeur
-                    
-                    
+    
+def minimax(board, depth, alpha, beta, maximizing_player):
+
+    board.is_human_turn = not maximizing_player 
+
+    children = board.get_all_possible_moves()
+
+    if depth == 0 or board.is_draw or board.is_check_mate:
+        return None, evaluate(board)
+
+    best_move = random.choice(children)
+
+    if maximizing_player:
+        max_eval = -math.inf
+        for child in children:
+            board_copy = copy.deepcopy(board)
+            board_copy.move(child[0][0], child[0][1], child[1][0], child[1][1])
+            current_eval = minimax(board_copy, depth - 1, alpha, beta, False)[1]
+            if current_eval > max_eval:
+                max_eval = current_eval
+                best_move = child
+            alpha = max(alpha, current_eval)
+            if beta <= alpha:
+                break
+        return best_move, max_eval
+
+    else:
+        min_eval = math.inf
+        for child in children:
+            board_copy = copy.deepcopy(board)
+            board_copy.move(child[0][0], child[0][1], child[1][0], child[1][1])
+            current_eval = minimax(board_copy, depth - 1, alpha, beta, True)[1]
+            if current_eval < min_eval:
+                min_eval = current_eval
+                best_move = child
+            beta = min(beta, current_eval)
+            if beta <= alpha:
+                break
+        return best_move, min_eval                    
                 
             
             
