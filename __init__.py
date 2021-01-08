b2dce10683330fa75d25f9cd0f7e3273d041b1a7
RICHARD-90 <angerichardjunior@gmail.com>
2021-01-08 05:04:49 +0100

    modification de alphabeta et correction de minmax

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import chess
+import math as m
 
-
+#==============================================================================
 def polyglot(board):
     position = list() # position dans polyglot
     #access the Polyglot book
@@ -8,54 +9,102 @@ def polyglot(board):
         for entry in reader.find_all(board):
             position.append((entry.move, entry.weight, entry.learn))
     return position
-#=================================================================
-def MineMax(etatcourant,prof,boolean):
-    if (prof==0 or etatcourant.is_game_over()):
-        return ponderation(etatcourant) #ne bouge pas
-    elif boolean==False:
-        valeur=-9999
-        for child in etatcourant.legal_moves :
-            etatcourant = etatcourant.push(chess.Move.from_uci(child))
-            valeur=max(valeur,MineMax(child,prof-1,False))
-        return valeur
+#==============================================================================
+def minmax(depth, board, is_maximizing):
+    # je copy le board
+    board_copy = board.copy()
+    if depth == 0 :
+        # on evalue le board
+        return ponderation(board_copy)
+    
+    possibleMoves = board_copy.legal_moves
+    
+    if is_maximizing:
+        v = -9999
+        for move in possibleMoves:
+            move = chess.Move.from_uci(str(move))
+            board_copy.push(move)
+            v = max(v, minmax(depth-1, board_copy, not(is_maximizing)))
+            board_copy.pop()
+        return v
     else:
-        valeur=9999
-        for child  in etatcourant.legal_moves:
-            etatcourant = etatcourant.push(chess.Move.from_uci(child))
-            valeur=min(valeur,MineMax(child,prof-1,True))
-        return valeur
-#=============================================================
-def alpha_betaA(profondeur, board, a, b):
-	if profondeur == 0:
-		return ponderation(board)
-	v = -9999
+        v = 9999
+        for move in possibleMoves:
+            move = chess.Move.from_uci(str(move))
+            board_copy.push(move)
+            v = min(v, minmax(depth-1, board_copy, not(is_maximizing)))
+            board_copy.pop()
+        return v
+#==============================================================================
+def alphabeta(depth, board, is_maximizing):
+	
 	bestmove = None
-	moves = board.legal_moves
-	for move in moves:
-		board.push(chess.Move.from_uci(str(move)))
-		v = max(v, alpha_betaB(profondeur - 1, board, a, b))
-		if v > b : 
-			bestmove = chess.Move.from_uci(move)
-			print(bestmove, v)
-			return v
-		a = max(v,a)
-		return v
+	movepond = -m.inf
+	print(bestmove, movepond)
 
-def alpha_betaB(profondeur, board, a, b):
-	if profondeur == 0:
-		return ponderation(board)
-	v = 9999
-	bestmove = None
-	moves = board.legal_moves
-	for move in moves:
-		board.push(chess.Move.from_uci(str(move)))
-		v = min(v, alpha_betaA(profondeur-1, board, a, b))
-		if v <= a :
-			bestmove = chess.Move.from_uci(move)
-			print(bestmove, v)()
-		b = min(v, b)
-	return v
-#============================================================
+	a = minmax(depth, board, is_maximizing)
+	b = minmax(depth, board, not(is_maximizing))
+	
+	board1 = board.copy()
+	moves = board1.legal_moves
+	for k in moves:
+		move = chess.Move.from_uci(str(k))
+		board1.push(move)
+
+		if is_maximizing:
+			valeur = alpha(depth, board1, b, a)
+		else:
+			valeur = beta(depth, board1, b, a)
+
+		board1.pop()
+		if valeur > movepond:
+			bestmove = move
+			movepond = valeur
+			print(bestmove, movepond)
+
+	return bestmove
+#==============================================================================
+def alpha(depth, board, a, b):
+    board1 = board.copy()
+    
+    if depth == 0:
+        return ponderation(board1)
+    valeur = - m.inf
+    moves = board1.legal_moves
+    try:
+        for move in moves:
+            x = chess.Move.from_uci(str(move))
+            try:
+                board1.push(x)
+            except AssertionError:
+                continue
+            valeur = max(valeur, beta(depth-1, board1, a, b))
+            if valeur >= b :
+                return valeur
+            a = max(valeur, a)
+    except IndexError:
+        pass
+    return valeur 
+
+def beta(depth, board, a, b):
+    board2 = board.copy()
+    
+    if depth == 0:
+        return ponderation(board2)
+    valeur = m.inf
+    moves = board2.legal_moves
+    for depl in moves:
+        k = chess.Move.from_uci(str(depl))
+        try:
+            board2.push(k)
+        except AssertionError:
+            continue
+        valeur = min(valeur, alpha(depth-1, board2, a, b))
+        if valeur <= a:
+            return valeur 
+        b = min(valeur, b)
+    return valeur
+#==============================================================================
 def get_piecevalue(piece):
     if piece == None:
         return 0
@@ -70,14 +119,33 @@ def get_piecevalue(piece):
     elif piece == 'q' or piece == 'Q':
         return 100
     elif piece == 'k' or piece == 'K':
-        return 1000
-    
-      
+        return 1000 
+#==============================================================================      
 def ponderation(board):
     #on parcourt les differentes cases du board 
     p = 0
     #chaque fois que le board s'actualise, la ponderation change
-    for i in (0,63):
-            p = p + get_piecevalue(str(board.piece_at(i)))
-        
+    
+    i = 0
+    
+    while i < 64 and get_piecevalue(str(board.piece_at(i))) != None:
+    # for i in (0,63):
+        p += get_piecevalue(str(board.piece_at(i)))
+        i += 1
     return p
+#==============================================================================
+# test
+# depth = 3
+# board = chess.Board()
+
+# print(board)
+# a = minmax(depth, board, True)
+# b = minmax(depth, board, False)
+# #print(a, b)
+# alphabeta(depth, board, True)
+# # print(alpha(depth, board, b, a))
+# # print(beta(depth, board, b, a))
+# board.push_san('g1f3')
+# print(alpha(depth, board, b, a))
+# alphabeta(depth, board, True)
+# # print(board)
